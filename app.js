0bf3f4979c0a07a8d8141fcda46464d1851d9769
duriali <abduri123@gmail.com>
2022-09-02 10:58:47 +0200

    env

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require("express");
+var cors = require('cors');
 const app = express();
 const { getDocument } = require("./db/database");
 
+app.use(cors());
 app.use(express.json());
 
 const notesRouter = require("./api/notes.router");
--- db/database.js
+++ db/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "MYsqlpassword1",
+    password: process.env.DATABASE_PW,
     database: "notes-app-schema",
     connectionLimit: 10,
     //   insecureAuth : true,
